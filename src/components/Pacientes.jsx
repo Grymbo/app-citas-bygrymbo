e5bd005a79eab287ed2ba4fa66fcac3287d5d26f
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-04-19 10:04:52 -0600

    Se erminan los diseños de losyado de pacientes y se agrega un nuevo componente Pacientes.jsx, para mejorar la eficiencia de ListadPacinetes

---
+++ src/components/Pacientes.jsx
@@ -0,0 +1,43 @@
+
+const Pacientes = () => {
+  return (
+    <div className=" m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+          <p className=" font-bold mb-3 text-gray-700 uppercase">
+            Nombre: {''}
+              <span className=" font-normal normal-case">
+                Cosi
+              </span>
+          </p>
+
+          <p className=" font-bold mb-3 text-gray-700 uppercase">
+            Nombre del Propietario: {''}
+              <span className=" font-normal normal-case">
+                Grymbo
+              </span>
+          </p>
+
+          <p className=" font-bold mb-3 text-gray-700 uppercase">
+            Email: {''}
+              <span className=" font-normal normal-case">
+                email@email.com
+              </span>
+          </p>
+
+          <p className=" font-bold mb-3 text-gray-700 uppercase">
+            Alta: {''}
+              <span className=" font-normal normal-case">
+                20 de Agosto del 2022
+              </span>
+          </p>
+
+          <p className=" font-bold mb-3 text-gray-700 uppercase">
+            Sintomas: {''}
+              <span className=" font-normal normal-case">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit optio facilis inventore nisi ullam repudiandae officia accusamus officiis laudantium laboriosam totam sed aut, sapiente quaerat temporibus ad delectus animi. Quibusdam.
+              </span>
+          </p>
+        </div>
+  )
+}
+
+export default Pacientes
