3e128b4390f1b1afc29504a4ad45eef9846e55ca
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-04-18 21:45:59 -0600

    Se termina el header, y se da estilizacion a componentes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,33 @@
 
 const Formulario = () => {
+
   return (
-    <div>
-        <h2>Formulario</h2>
+    <div className=" md:w-1/2 lg:w-2/5">
+
+      <h2 className=" font-black text-3xl text-center">
+        Seguimiento de Mascotas
+      </h2>
+
+      <p className=" text-lg mt-5 text-center mb-10">
+        Añade Pacientes y {''}
+          <span className=" text-indigo-600 font-bold">Administralos</span>
+      </p>
+
+      <form action="" className=" bg-white shadow-md rounded-lg py-10 px-5">
+        <div>
+          <label htmlFor="Mascota" className=" block text-gray-700 uppercase">
+              Nombre Mascota
+          </label>
+
+          <input 
+            id="Mascota"
+            className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            type="text"
+            placeholder="Nombre de la Mascota" 
+          />
+        </div>
+      </form>
+
     </div>
   )
 }
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,8 +1,10 @@
 
 const ListadoPacientes = () => {
   return (
-    <div>
-        <h2>ListadoPacientes</h2>
+    <div className=" md:w-1/2 lg:w-3/5">
+        <h2>
+          ListadoPacientes
+        </h2>
     </div>
   )
 }
