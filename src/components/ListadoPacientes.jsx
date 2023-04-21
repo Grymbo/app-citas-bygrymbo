ab195afde743aa7c390196b3f336c98324679bb2
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-04-21 16:42:25 -0600

    Se agregan los botones de editar y eliminar

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -4,23 +4,45 @@ const ListadoPacientes = ({pacientes}) => {
 
   return (
     <div className=" md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll mx-5">
-        <h2 className=" font-black text-3xl text-center">
-          ListadoPacientes
-        </h2>
 
-        <p className=" text-xl mt-5 mb-10 text-center">
-          Administra tus {''}
-            <span className=" text-indigo-600 font-bold">
-              Mascotas y Citas
-            </span>
-        </p>
+        {pacientes && pacientes.length ? (
+          
+          <>
+            <h2 className=" font-black text-3xl text-center">
+              ListadoPacientes
+            </h2>
 
-        {pacientes.map((paciente) => (
-          <Paciente
-            key={paciente.id}
-            paciente={paciente}
-          />
-        ))}
+            <p className=" text-xl mt-5 mb-10 text-center">
+              Administra tus {''}
+                <span className=" text-indigo-600 font-bold">
+                  Mascotas y Citas
+                </span>
+            </p>
+
+            {pacientes.map((paciente) => (
+              <Paciente
+                key={paciente.id}
+                paciente={paciente}
+              />
+            ))}
+          </>
+
+        ) : (
+          <>
+            <h2 className=" font-black text-3xl text-center">
+              No hay pacientes
+            </h2>
+
+            <p className=" text-xl mt-5 mb-10 text-center">
+              Comienza agregando {''}
+                <span className=" text-indigo-600 font-bold">
+                  nuevas Mascotas y Citas
+                </span>
+            </p>
+          </>
+        )}
+
+        
 
         
         
